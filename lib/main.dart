dc2329e338ac391479e359db32769c279af582bd
zainalsalamun <zainalrtf@gmail.com>
2025-12-18 13:26:02 +0700

    update : route page location

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_flash_coffe/page/bottom/bottom_nav_bar.dart';
 import 'package:flutter_flash_coffe/page/home/home_page.dart';
 import 'package:flutter_flash_coffe/page/home/menu_page.dart';
 import 'package:flutter_flash_coffe/page/onboarding/onboarding_page.dart';
+import 'package:flutter_flash_coffe/page/registration/location_permission_page.dart';
 import 'package:flutter_flash_coffe/page/registration/otp_page.dart';
 import 'package:flutter_flash_coffe/page/registration/signup_login_page.dart';
 import 'package:flutter_flash_coffe/page/splash_screen/splash_screen.dart';
@@ -31,6 +32,7 @@ class MyApp extends StatelessWidget {
         '/promo': (context) => const MenuPage(),
         '/profile': (context) => const MenuPage(),
         '/bottom': (context) => const BottomNavScaffold(),
+        '/location-permission': (context) => const PageLocationPermission(),
       },
     );
   }
