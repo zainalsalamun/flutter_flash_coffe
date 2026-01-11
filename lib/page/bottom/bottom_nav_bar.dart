4b4c2a2520627e4ccd9dadb39c012d79d9ea9641
zainalsalamun <zainalrtf@gmail.com>
2026-01-11 15:59:59 +0700

    update : bottom bar

--- lib/page/bottom/bottom_nav_bar.dart
+++ lib/page/bottom/bottom_nav_bar.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_flash_coffe/constant/color_constant.dart';
 import 'package:flutter_flash_coffe/page/home/home_page.dart';
 import 'package:flutter_flash_coffe/page/home/menu_page.dart';
+import 'package:flutter_flash_coffe/page/promo/promo_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class BottomNavScaffold extends StatefulWidget {
   const BottomNavScaffold({super.key});
@@ -16,8 +18,8 @@ class _BottomNavScaffoldState extends State<BottomNavScaffold> {
   final List<Widget> _pages = [
     const HomePage(isDelivery: false),
     const MenuPage(),
-    Center(child: Text("Promo")),
-    Center(child: Text("Profile")),
+    const PromoPage(),
+    const Center(child: Text("Profile")),
   ];
 
   @override
@@ -26,17 +28,39 @@ class _BottomNavScaffoldState extends State<BottomNavScaffold> {
       body: IndexedStack(index: _currentIndex, children: _pages),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
-        selectedItemColor: ColorConstant.primary,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        selectedItemColor: ColorConstant.primaryButton,
         unselectedItemColor: Colors.grey,
+        selectedLabelStyle: GoogleFonts.inter(
+          fontWeight: FontWeight.bold,
+          fontSize: 12,
+        ),
+        unselectedLabelStyle: GoogleFonts.inter(
+          fontWeight: FontWeight.w500,
+          fontSize: 12,
+        ),
         onTap: (index) => setState(() => _currentIndex = index),
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.menu), label: "Menu"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: "Home",
+            activeIcon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.description_outlined),
+            activeIcon: Icon(Icons.description),
+            label: "Menu",
+          ),
           BottomNavigationBarItem(
             icon: Icon(Icons.card_giftcard),
             label: "Promo",
           ),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            activeIcon: Icon(Icons.person),
+            label: "Profile",
+          ),
         ],
       ),
     );
