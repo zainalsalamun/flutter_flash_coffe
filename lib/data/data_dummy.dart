9eadff6b67410908722d163ae962d1e5f05b9e52
zainalsalamun <zainalrtf@gmail.com>
2026-01-12 10:16:50 +0700

    update : data dummy image assets menu

--- lib/data/data_dummy.dart
+++ lib/data/data_dummy.dart
@@ -4,13 +4,13 @@ class DataDummy {
   List<Product> recommendedProducts = [
     Product(
       name: "Choco Fudge Frappe",
-      image: "assets/svg/menu/choco_fudge_frappe.svg",
+      image: "assets/images/menu/choco_fudge_frappe.png",
       price: 24000,
       tag: "Rekomendasi",
     ),
     Product(
       name: "Charcoal Latte",
-      image: "assets/svg/menu/charcoal_latte.svg",
+      image: "assets/images/menu/charcoal_latte.png",
       price: 26000,
       tag: "Rekomendasi",
     ),
@@ -19,22 +19,22 @@ class DataDummy {
   List<Product> frappeSeries = [
     Product(
       name: "Choco Fudge Frappe",
-      image: "assets/svg/menu/choco_fudge_frappe.svg",
+      image: "assets/images/menu/choco_fudge_frappe.png",
       price: 28000,
     ),
     Product(
       name: "Aren Frappe",
-      image: "assets/svg/menu/aren_frappe.svg",
+      image: "assets/images/menu/aren_frappe.png",
       price: 32000,
     ),
     Product(
       name: "Matcha Frappe",
-      image: "assets/svg/menu/matcha_frappe.svg",
+      image: "assets/images/menu/matcha_frappe.png",
       price: 28000,
     ),
     Product(
       name: "Choco Hazelnut Frappe",
-      image: "assets/svg/menu/choco_hazelnut_frappe.svg",
+      image: "assets/images/menu/choco_hazelnut_frappe.png",
       price: 32000,
     ),
   ];
@@ -42,20 +42,100 @@ class DataDummy {
   List<Product> signatureLattes = [
     Product(
       name: "Charcoal Latte",
-      image: "assets/svg/menu/charcoal_latte.svg",
+      image: "assets/images/menu/charcoal_latte.png",
       price: 26000,
     ),
     Product(
       name: "Brown Sugar Latte",
-      image: "assets/svg/menu/brown_sugar_latte.svg",
+      image: "assets/images/menu/brown_sugar_latte.png",
       price: 28000,
     ),
   ];
+  List<Product> milkshake = [
+    Product(
+      name: "Marie Regal Milkshake",
+      image: "assets/images/menu/marie_regal.png",
+      price: 28000,
+    ),
+    Product(
+      name: "Lotus Biscoff Milkshake",
+      image: "assets/images/menu/lotus_biscoff.png",
+      price: 32000,
+    ),
+    Product(
+      name: "Matcha Macadamia",
+      image: "assets/images/menu/matcha_macadamia.png",
+      price: 32000,
+    ),
+  ];
+
+  List<Product> tea = [
+    Product(
+      name: "Lychee Tea",
+      image: "assets/images/menu/lychee_tea.png",
+      price: 22000,
+    ),
+    Product(
+      name: "Peach Tea",
+      image: "assets/images/menu/peach_tea.png",
+      price: 22000,
+    ),
+    Product(
+      name: "Lemon Tea",
+      image: "assets/images/menu/lemon_tea.png",
+      price: 20000,
+    ),
+  ];
+
+  List<Product> food = [
+    Product(
+      name: "Butter Croissant",
+      image: "assets/images/menu/butter_croissant.png",
+      price: 24000,
+    ),
+    Product(
+      name: "Almond Croissant",
+      image: "assets/images/menu/almond_croissant.png",
+      price: 28000,
+    ),
+    Product(
+      name: "Pain Au Chocolat",
+      image: "assets/images/menu/pain_au_chocolat.png",
+      price: 26000,
+    ),
+    Product(
+      name: "Tuna Puff",
+      image: "assets/images/menu/tuna_puff.png",
+      price: 28000,
+    ),
+  ];
+
+  List<Product> snack = [
+    Product(
+      name: "Choco Chip Cookie",
+      image: "assets/images/menu/choco_chip.png",
+      price: 18000,
+    ),
+    Product(
+      name: "Oatmeal Raisin",
+      image: "assets/images/menu/oatmeal_raisin.png",
+      price: 18000,
+    ),
+    Product(
+      name: "Macadamia Cookie",
+      image: "assets/images/menu/macadamia.png",
+      price: 22000,
+    ),
+  ];
+
   List<String> banners = [
-    "assets/banner/ic_mission_banner.svg",
-    "assets/banner/ic_promo_flash_coffee.svg",
-    "assets/banner/ic_voucher_banner.svg",
-    "assets/banner/ic_profile_banner.svg",
-    "assets/banner/ic_voucher_banner_.svg",
+    "assets/banner/home_banner_flash_sale.png",
+    "assets/banner/home_banner_voucher_5x25.png",
+    "assets/banner/ic_mission_banner.png",
+    "assets/banner/ic_promo_flash_coffee.png",
+    "assets/banner/ic_voucher_banner_yellow.png",
+    "assets/banner/ic_voucher.png",
+    "assets/banner/ic_profile_banner_promo.png",
+    "assets/banner/ic_profile_banner.png",
   ];
 }
